6cfdd13691af4f09fdff6e2c1762a3c0055d6957
Mohamed Zayan <m7mdzayan@gmail.com>
2024-01-24 16:18:34 +0200

    💄 add colors and the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-bg': 'var(--dark-bg)',
+        'dark-card-bg': '--dark-card-bg)',
+        white: {
+          500: 'var(--white-500)'
+        },
+        danger: {
+          50: 'var(--danger-50)',
+          100: 'var(--danger-100)',
+          300: 'var(--danger-300)',
+          400: 'var(--danger)'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
