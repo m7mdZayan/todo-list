c9f8a4ed561d35fda107d6df7c9c6092d8cb0ad5
Mohamed Zayan <m7mdzayan@gmail.com>
2024-01-24 15:54:50 +0200

    📦️ add ant design,tailwind css,es lint and prettier packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 const App = () => {
-    return (<></> );
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  )
 }
- 
-export default App;
+
+export default App
